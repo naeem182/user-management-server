c848130152f4c947ef4d371e2dfecc13b84b047e
naeem <naeemislamjr182@gmail.com>
2023-10-12 14:30:54 +0600

    array obj set in server

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+    { id: 1, name: 'John', email: 'john@example.com' },
+    { id: 2, name: 'Alice', email: 'alice@example.com' },
+    { id: 3, name: 'Bob', email: 'bob@example.com' },
+    { id: 4, name: 'Eva', email: 'eva@example.com' }
+];
 
 
 app.get('/', (req, res) => {
     res.send('user management server is running')
+})
+app.get('/users', (req, res) => {
+    res.send(users)
 })
 
 app.listen(port, () => {
